db97af8f0b1d9355f530f1dc188771a7520c73be
prsnt <prshntksinku7@gmail.com>
2025-03-26 17:29:47 +0530

    modified cors for port 8080 admin

--- server.js
+++ server.js
@@ -6,11 +6,24 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(cookieParser());
-app.use(cors({
-    origin: "http://localhost:3000",
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
-}));
+const allowedOrigins = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+  ];
+  
+  app.use(
+    cors({
+      origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          callback(new Error("Not allowed by CORS"));
+        }
+      },
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true
+    })
+  );
 
 app.use('/api',api);
 
