d64ca02fecebcec82ac605fcf13ed8fbfd61d697
prsnt <prshntksinku7@gmail.com>
2025-03-30 12:51:51 +0530

    proper redirect after google login, logout redirects to /

--- api/v1/controllers/passportController.js
+++ api/v1/controllers/passportController.js
@@ -2,11 +2,23 @@ import express from 'express';
 import passport from '../services/passportService.js';
 const router = express.Router();
 
-router.get('/google', passport.authenticate('google', {
-    scope:
-        ['email', 'profile'],
-    prompt: 'select_account',
-}));
+router.get('/google',
+    (req, res, next) => {
+        const redirect = req.query.redirect || "/"; // Default to home if no redirect is provided
+        console.log("got redirect in query : -",redirect);
+        res.cookie("redirectURL", redirect, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "Lax",
+            maxAge: 5 * 60 * 1000, // 5 minutes (so it doesn't persist too long)
+        });
+        next();
+    }
+    , passport.authenticate('google', {
+        scope:
+            ['email', 'profile'],
+        prompt: 'select_account',
+    }));
 
 router.get('/google/callback', passport.authenticate('google', {
     session: false,
@@ -21,27 +33,33 @@ router.get('/google/callback', passport.authenticate('google', {
 
     // ✅ Set an HTTP-only secure cookie
     res.cookie("token", token, {
-        httpOnly: true,  
+        httpOnly: true,
         secure: process.env.NODE_ENV === "production",
         sameSite: "Lax",
         maxAge: 3600000, // 1 hour expiration
     });
 
+    const redirect = req.cookies.redirectURL;
+    console.log("redirecting after succ auth google ", redirect);
+    res.clearCookie("redirectUrl");
     // ✅ Redirect to your frontend (Next.js)
-    res.redirect(`${process.env.MEDCARE_BASE_URL}/landingPage`);
+    res.redirect(`${process.env.MEDCARE_BASE_URL}${redirect}`);
 
 });
 
 router.post("/logout", (req, res) => {
     res.cookie("token", "", {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production", 
+        secure: process.env.NODE_ENV === "production",
         sameSite: "Strict",
         expires: new Date(0), // 🔥 Expire immediately
         path: "/", // ✅ Ensures cookie is cleared for all paths
     });
 
-    return res.status(200).json({ message: "Logged out successfully" });
+    return res.status(200).json({
+        message: "Logged out successfully",
+        redirectUrl: process.env.MEDCARE_BASE_URL || "/"
+    });
 });
 
 export default router;
